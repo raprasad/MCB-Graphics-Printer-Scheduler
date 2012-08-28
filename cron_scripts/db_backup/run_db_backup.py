a1b0c8de8c2bd0ddfefee32f02b28dd6ff193888
Raman Prasad <raman_prasad@harvard.edu>
2012-08-28 10:02:54 -0400

    db backup scripts

---
+++ cron_scripts/db_backup/run_db_backup.py
@@ -0,0 +1,18 @@
+import os, sys
+
+sys.path.append('/home/p/r/prasad/webapps/django/MCB-Graphics-Printer-Scheduler/mcb_printer_scheduler')
+sys.path.append('/home/p/r/prasad/webapps/django/MCB-Graphics-Printer-Scheduler')
+
+from finance_utilities import settings
+from django.core.management import setup_environ
+setup_environ(settings)
+
+# pull in "BackupMaker"
+sys.path.append('/home/p/r/prasad/webapps/django/mcb_lib/poor-mans-db-backup')
+from backupdb.backup_files import BackupMaker
+
+
+if __name__ == '__main__':
+    mb = BackupMaker(backup_name='Poster Printer')
+    mb.make_backup()
+
