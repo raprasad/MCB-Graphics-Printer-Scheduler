7dc47b0074fba9877f2bebc80ae18574ffaa9cd3
eric02138 <ericmattison@Erics-MacBook-Pro-2.local>
2014-09-29 15:46:41 -0400

    Eric's poster printer changes, some files missing

--- mcb_printer_scheduler/config/desktop_authz.py
+++ mcb_printer_scheduler/config/desktop_authz.py
@@ -179,41 +179,6 @@ LOGGING = {
     }
 }
 
-"""
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'formatters': {
-        'verbose': {
-            'format' : "[%(asctime)s] %(levelname)s [%(name)s:%(lineno)s] %(message)s",
-            'datefmt' : "%d/%b/%Y %H:%M:%S"
-        },
-        'simple': {
-            'format': '%(levelname)s %(message)s'
-        },
-    },
-    'handlers': {
-        'file': {
-            'level': 'DEBUG',
-            'class': 'logging.FileHandler',
-            'filename': 'error.log',
-            'formatter': 'verbose'
-        },
-    },
-    'loggers': {
-        'django': {
-            'handlers':['file'],
-            'propagate': True,
-            'level':'DEBUG',
-        },
-        'reservation_history': {
-            'handlers': ['file'],
-            'level': 'DEBUG',
-        },
-    }
-}
-"""
-
 POORMANS_DB_BACKUP_DIR = 'use for prod mysql backups'
 PRINT_PROOFING_COST = '0.6'
 TAX_RATE = '0.065'
--- mcb_printer_scheduler/reservation_history/views.py
+++ mcb_printer_scheduler/reservation_history/views.py
@@ -1,3 +1,5 @@
+import re
+
 from django.http import HttpResponse, Http404, HttpResponseRedirect
 from django.template import RequestContext
 from django.shortcuts import render_to_response, render, redirect, get_object_or_404
@@ -141,26 +143,32 @@ def view_create_invoice(request, res_id):
         if frm.is_valid():
             frm_data = frm.cleaned_data
             frm_data['note'] = mark_safe(frm_data['note'].replace('\n', '<br />\n'))
-            frm_data['note'] = mark_safe("%s<br />\n%d sq. feet" % (frm_data['note'], frm_data['sq_feet']))
+            frm_data['note'] = mark_safe("%s<br />\n" % frm_data['note'])
             frm_data['extra'] = mark_safe(frm_data['extra'].replace('\n', '<br />\n'))
             prod_cost = frm_data['print_media'].name
             billing_code = frm_data['billing_code']
+            poster_tube = PosterTubeType.objects.get(pk=frm_data['poster_tube'])
+            cc = frm_data['cc']
             if frm_data['payment_method'] == 'cash':
                 billing_code = 'paid with cash'
             lu.update({'fd': frm_data,
+                       'cc': cc,
                        'billing_code': billing_code,
-                       'prod_cost': prod_cost })
+                       'prod_cost': prod_cost,
+                       'poster_tube': poster_tube })
             if frm_data['download_pdf'] == "True":
                 return view_save_invoice(request, lu)
             return view_preview_invoice(request, lu)
         return render_to_response('reservation_history/view_form_create_invoice.html',
                                   lu, context_instance=RequestContext(request))
     else:
-        note_array = [reservation.lab_name, reservation.note, reservation.poster_tube_details]
+        note_array = [reservation.lab_name, reservation.note]
         note_array = filter(None, note_array)
         note_string = '\n'.join(note_array)
         initial_form_data = {'note': note_string,
-                             'last_update': reservation.last_update,
+                             'billing_date': datetime.now(),
+                             'completed_date': reservation.last_update,
+                             'print_proofing_cost': PRINT_PROOFING_COST,
                              'sq_feet': 0.0}
         if reservation.print_media != None:
             initial_form_data.update({'print_media': reservation.print_media})
@@ -196,8 +204,7 @@ def view_save_invoice(request, lu):
                       html=html)
     invoice.save()
     
-    return download_invoice(request, invoice_no, filename)
-    #return redirect(reverse('view_reservation_history', kwargs={'id_hash': reservation.user.id_hash}))
+    return download_invoice(request, filename, lu)
 
 def view_display_invoice(request, invoice_no):
     invoice = get_object_or_404(Invoice, invoice_no=invoice_no)
@@ -205,9 +212,9 @@ def view_display_invoice(request, invoice_no):
     return render_to_response('reservation_history/view_display_invoice.html',
                               lu, context_instance=RequestContext(request))
 
-def download_invoice(request, invoice_no, filename):
+def download_invoice(request, filename, lu):
 
-    lu = get_common_lookup(request)
+    #lu = get_common_lookup(request)
     cal_user = lu.get('calendar_user', None)
 
     if cal_user.is_calendar_admin:
@@ -217,15 +224,14 @@ def download_invoice(request, invoice_no, filename):
         return render_to_response('reservation_history/view_user_history.html', 
                                   lu, context_instance=RequestContext(request))
 
-
-    invoice = get_object_or_404(Invoice, invoice_no=invoice_no)
+    invoice = get_object_or_404(Invoice, invoice_no=lu['invoice_no'])
 
     #first delete previous invoices
     cmd = 'rm -f /tmp/*.pdf'
     p = Popen(cmd, stdout=PIPE, shell=True)
     output = p.communicate()
 
-    url = 'http://localhost/poster-printer/history/display-invoice/%s/' % (invoice_no)
+    url = 'http://localhost/poster-printer/history/display-invoice/%s/' % (lu['invoice_no'])
     filepath = '/tmp/%s' % filename
     cmd  = '/usr/local/bin/wkhtmltopdf.sh %s %s' % (url, filepath)
     p = Popen(cmd, stdout=PIPE, shell=True)
@@ -245,10 +251,16 @@ def download_invoice(request, invoice_no, filename):
     htmly     = get_template('email/download_pdf.html')
     d = Context(lu)
     subject = 'Invoice copy from MCB Graphics'
-    from_email, to = MCB_GRAPHICS_EMAIL, cal_user.get_email()
+    from_email = MCB_GRAPHICS_EMAIL
+    to = [cal_user.get_email()]
+    email_separator = re.compile(r'[,;]+')
+    cc_list = email_separator.split(lu['cc'])
+    for email in cc_list:
+        to.append(email)
+
     text_content = plaintext.render(d)
     html_content = htmly.render(d)
-    msg = EmailMultiAlternatives(subject, text_content, from_email, [to])
+    msg = EmailMultiAlternatives(subject, text_content, from_email, to)
     msg.attach_alternative(html_content, "text/html")
     msg.attach_file(filepath)
     #msg.send()
